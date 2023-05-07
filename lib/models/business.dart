51e17b626d64deaab5814c3a72b2951950aa6b28
Noé Muñoz Pérez <sightceo@gmail.com>
2023-05-07 00:51:36 -0600

    Refactored business.dart factory method fromJSON to use added private constructor.

--- lib/models/business.dart
+++ lib/models/business.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 class Business {
   int id = 0;
-  String name = '';
+  String name = "";
   String brand = "";
   String taxID = "";
   String slogan = "";
@@ -10,7 +10,7 @@ class Business {
   String oneLiner = "";
   String logoURL =
       'https://my.alvyss.com/api/v1/cloudron/avatar?2395601373707481';
-  String industry = '';
+  String industry;
   Business(
       {required this.id,
       required this.name,
@@ -21,21 +21,42 @@ class Business {
       String? oneLiner,
       String? logoURL,
       required this.industry});
-  Business.fromJSON(Map<String, dynamic> data) {
-    id = data['id'] as int;
+  Business._(
+      {required this.id,
+      required this.name,
+      this.brand = '',
+      this.taxID = '',
+      this.slogan = '',
+      this.tagLine = '',
+      this.oneLiner = '',
+      this.logoURL =
+          'https://my.alvyss.com/api/v1/cloudron/avatar?2395601373707481',
+      required this.industry});
+  factory Business.fromJSON(Map<String, dynamic> data) {
+    var id = data['id'] as int;
     if (id.isNaN) {
       throw UnsupportedError('Invalid data: $data -> "id" is not a number');
     } else if (id.isNegative) {
       throw UnimplementedError(
           'Invalid data: $data -> "id" cannot be negative');
     }
-    name = utf8.decode(utf8.encode(data['name'] as String));
-    brand = utf8.decode(utf8.encode(data['brand'] as String? ?? ''));
-    taxID = utf8.decode(utf8.encode(data['tid'] as String? ?? ''));
-    slogan = utf8.decode(utf8.encode(data['slogan'] as String? ?? ''));
-    tagLine = utf8.decode(utf8.encode(data['tag-line'] as String? ?? ''));
-    oneLiner = utf8.decode(utf8.encode(data['one-liner'] as String? ?? ''));
-    logoURL = utf8.decode(utf8.encode(data['logo-url'] as String? ?? ''));
-    industry = utf8.decode(utf8.encode(data['industry'] as String));
+    var name = utf8.encode(data['name'] as String);
+    var brand = utf8.encode(data['brand'] as String? ?? '');
+    var taxID = utf8.encode(data['tid'] as String? ?? '');
+    var slogan = utf8.encode(data['slogan'] as String? ?? '');
+    var tagLine = utf8.encode(data['tag-line'] as String? ?? '');
+    var oneLiner = utf8.encode(data['one-liner'] as String? ?? '');
+    var logoURL = utf8.encode(data['logo-url'] as String? ?? '');
+    var industry = utf8.encode(data['industry'] as String);
+    return Business._(
+        id: id,
+        name: utf8.decode(name),
+        brand: utf8.decode(brand),
+        taxID: utf8.decode(taxID),
+        slogan: utf8.decode(slogan),
+        tagLine: utf8.decode(tagLine),
+        oneLiner: utf8.decode(oneLiner),
+        logoURL: utf8.decode(logoURL),
+        industry: utf8.decode(industry));
   }
 }
